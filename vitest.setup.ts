c5194a90c05e2a69a6d94bf5376e2cae71df2d7e
zhensherlock <zhensherlock@126.com>
2025-07-14 14:50:50 +0800

    test: setup vitest with client lifecycle hooks

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,19 +1,25 @@
 import 'dotenv/config'
+import { afterAll, beforeAll } from 'vitest'
 import { Client } from '@modelcontextprotocol/sdk/client/index.js'
 import { StdioClientTransport } from '@modelcontextprotocol/sdk/client/stdio.js'
 
-const serverParams = new StdioClientTransport({
-  command: 'c8',
-  args: ['--reporter=lcov', '--reporter=text', 'tsx', './src/index.ts'],
-  env: {
-    ...process.env,
-    NODE_V8_COVERAGE: './coverage/tmp',
-  },
+beforeAll(async () => {
+  const serverParams = new StdioClientTransport({
+    command: 'c8',
+    args: ['--reporter=lcov', '--reporter=text', 'tsx', './src/index.ts'],
+    env: {
+      ...process.env,
+      NODE_V8_COVERAGE: './coverage/tmp',
+    },
+  })
+  const client = new Client({
+    name: 'doc-mcp-client',
+    version: '1.0.0',
+  })
+  await client.connect(serverParams)
+  global.client = client
 })
-const client = new Client({
-  name: 'doc-mcp-client',
-  version: '1.0.0',
-})
-await client.connect(serverParams)
 
-global.client = client
+afterAll(async () => {
+  await global.client.close()
+})
