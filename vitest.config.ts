2e2510bb502608ac7a0634185283b8cdb4f86fd2
zhensherlock <zhensherlock@126.com>
2025-07-18 22:00:38 +0800

    refactor: remove alias config from vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    alias: {
-      '@': '/src',
-    },
     setupFiles: ['./vitest.setup.ts'],
     globalSetup: ['./vitest.global.ts'],
     coverage: {
